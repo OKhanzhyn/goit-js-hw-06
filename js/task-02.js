b24754732436f76bac3cb5b24226368aa82425bd
OKhanzhyn <dev.khan.world@gmail.com>
2023-09-30 19:35:53 +0300

    JS_HT6_ver2

--- js/task-02.js
+++ js/task-02.js
@@ -9,12 +9,34 @@ const ingredients = [
 ];
 const listOfIngredients = document.querySelector('#ingredients');
 
- ingredients.forEach(function (ingredients) {
-  const liElement = document.createElement('li');
-  liElement.textContent = ingredients;
-  liElement.classList.add('item');
-  listOfIngredients.append(liElement);  
-}); 
+ const firstLiElement = document.createElement('li');
+ firstLiElement.textContent = ('Potatoes');
+ firstLiElement.classList.add('item');
+ const secondLiElement = document.createElement('li');
+ secondLiElement.textContent = ('Mushrooms');
+ secondLiElement.classList.add('item');
+ const therdLiElement = document.createElement('li');
+ therdLiElement.textContent = ('Garlic');
+ therdLiElement.classList.add('item');
+ const forthLiElement = document.createElement('li');
+ forthLiElement.textContent = ( 'Tomatos');
+ forthLiElement.classList.add('item');
+ const fifthLiElement = document.createElement('li');
+ fifthLiElement.textContent = ('Herbs');
+ fifthLiElement.classList.add('item');
+ const sixLiElement = document.createElement('li');
+ sixLiElement.textContent = ('Condiments');
+ sixLiElement.classList.add('item');
+ listOfIngredients.append(firstLiElement, secondLiElement,therdLiElement, forthLiElement, fifthLiElement, sixLiElement);  
+ console.log(listOfIngredients);
 
-console.log(listOfIngredients);
+ 
+//  ingredients.forEach(function (ingredients) {
+//   const liElement = document.createElement('li');
+//   liElement.textContent = ingredients;
+//   liElement.classList.add('item');
+//   listOfIngredients.append(liElement);  
+// }); 
+
+
 
